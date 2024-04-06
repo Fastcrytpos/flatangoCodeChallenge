92b5ba0c7ab4f1c426e8e9f3e53890d893e8aaf8
Samson Mubeya <rasspartah@gmail.com>
2024-04-06 22:32:17 +0300

    did my assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Your code here
-let url = 'https://api.npoint.io/f8d1be198a18712d3f29/films/'
-console.log(document.getElementById('films'))
+let url = "http://localhost:3000/films"
+//console.log(document.getElementById('films'))
 
 
 document.getElementsByClassName('film item')[0].remove()
@@ -14,89 +14,111 @@ function fetchMovies(url){
     .then(movies => {
         movies.forEach(movie => {
             displayMovie(movie)
-            console.log(movie)
+           // console.log(movie)
         });
     })
 }
 
-let movieTicketsSold=0;
+// function reduceavailabletickets('http://localhost:3000/films/${movi.id}'){
+//     method: "PATCH",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//         "available_tickets": 0
+//     })
+//     .then((response) => response.json()) // Parse response as JSON
+//     .then((data) => console.log("Data updated successfully:", data))
+//     .catch((error) => console.error("Error updating data:", error));
 
+// }
+
+
+
+
+
+let movieTicketsSold=0;
+let movieCapacity=0;
+let pointsf;
+
+
+//to display list of movies
 function displayMovie(movie) {
     let movi = document.createElement("li");
     movi.textContent = movie.title;
-    movi.dataset.runtime = movie.runtime; // Storing runtime in a data attribute
-    movi.dataset.capacity = movie.capacity; // Storing capacity in a data attribute
-    movi.dataset.description = movie.description; // Storing description in a data attribute
-    movi.dataset.showtime = movie.showtime; // Storing showtime in a data attribute
-    movi.dataset.ticketsSold = movie.tickets_sold; // Storing tickets_sold in a data attribute
-    movi.dataset.poster = movie.poster; // Storing tickets_sold in a data attribute
-
-    movi.addEventListener("click", updatetherest);
+    movi.runtime = movie.runtime; 
+    movi.capacity = movie.capacity; 
+    movi.description = movie.description; 
+    movi.showtime = movie.showtime; 
+    movi.ticketsSold = movie.tickets_sold; 
+    movi.poster = movie.poster; 
     movi.className = "filmitem";
-    movi.id = movie.title;
+    movi.id = movie.id;
     document.getElementById("films").appendChild(movi);
+    movi.addEventListener("click", updatetherest);
 }
 
+//to add an event listener to every movie
 function updatetherest(event) {
+    pointsf=event.target
+    //console.log(pointsf.capacity)
     const movieTitle = event.target.textContent;
-    const movieruntime = event.target.dataset.runtime; // Accessing runtime from data attribute
-    const movieCapacity = event.target.dataset.capacity; // Accessing capacity from data attribute
-    const movieDescription = event.target.dataset.description; // Accessing description from data attribute
-    const movieShowtime = event.target.dataset.showtime; // Accessing showtime from data attribute
-    movieTicketsSold = event.target.dataset.ticketsSold; // Accessing tickets_sold from data attribute
-    const moviePoster = event.target.dataset.poster; // Accessing poster from data attribute
+    const movieruntime = event.target.runtime; 
+    movieCapacity = event.target.capacity; 
+    const movieDescription = event.target.description; 
+    const movieShowtime = event.target.showtime; 
+    movieTicketsSold = event.target.ticketsSold; 
+    const moviePoster = event.target.poster; 
     
-    alert("You clicked the movie with title: " + movieTitle);
+    //alert("You clicked the movie with title: " + movieTitle);
     document.getElementById("title").textContent = ("["+movieTitle+"]")
-
-
-
-
-
-    console.log("Movie Runtime: " + movieruntime);
     document.getElementById("runtime").textContent = "["+movieruntime+"] minutes"
-
-
-
-
-    console.log("Movie Capacity: " + movieCapacity);
-    document.getElementById("runtime").textContent = "["+movieruntime+"] minutes"
-
-
-
-
-    console.log("Movie Description: " + movieDescription);
     document.getElementById("film-info").textContent = movieDescription
-
-
-
-
-
-
-
-    console.log("Movie Showtime: " + movieShowtime);
     document.getElementById("showtime").textContent = ("["+movieShowtime+"]")
-
-
-
-
-
-
-    console.log("Movie Tickets Sold: " + movieTicketsSold);
-    document.getElementById("ticket-num").textContent = ("["+movieTicketsSold+"]")
-
+    document.getElementById("ticket-num").textContent = ("["+(movieCapacity-movieTicketsSold)+"]")
     document.getElementById("poster").src = moviePoster
-
-
 }
 
+//to add an event listener to the butto
+document.getElementById("buy-ticket").addEventListener("click", updatebtn)
+function updatebtn(){
 
-document.getElementById("buy-ticket").addEventListener("click", () => {
-    if(movieTicketsSold>1){
-    movieTicketsSold -=1;
-    document.getElementById("ticket-num").textContent = "[" + movieTicketsSold + "]";}
-    else(document.getElementById("ticket-num").textContent = "[" + "SOLD OUT!!" + "]")
-});
+    alert("Please note that on every click you are updating the server and its not reversible. so ukifanya test keep track ya movie umeselect")
+    let ticketssoldsupdate={
+        "id":pointsf.id,
+        "tickets_sold":movieTicketsSold+1
+    }
+    //console.log(pointsf.id)
+    console.log(ticketssoldsupdate)
+
+    //to update the server
+    fetch(url + '/' + pointsf.id, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(ticketssoldsupdate)
+      })
+        .then(response => response.json()) 
+        .then(data => console.log("Tickets updated successfully:", data))
+        .catch(error => console.error("Error updating tickets:", error));
+
+
+
+
+    //to update the available tickets for the movie after sale
+    let availableTickets =movieCapacity-movieTicketsSold
+    if(availableTickets>1){
+        movieTicketsSold +=1;
+        availableTickets -=1;
+
+        //console.log("local ticketsold"+pointsf.ticketsSold)
+
+       // this only for the button
+        document.getElementById("ticket-num").textContent = "[" + availableTickets + "]";}
+    else ((document.getElementById("ticket-num").textContent = "[" + "SOLD OUT!!" + "]")
+          (console.log("SOLD OUT!!")))}
+
 
 
 
